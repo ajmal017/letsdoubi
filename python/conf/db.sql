f127af55bd4d74d95ebe36269fb983e984dba3ec
BurningEgg <yellowcup333@gmail.com>
2018-06-12 00:41:23 -0400

    fix imports bug in db_manager and expand function for get_count_by_period

--- python/conf/db.sql
+++ python/conf/db.sql
@@ -2,3 +2,6 @@
 
 CREATE DATABASE doubi；
 CREATE TABLE Demo(id BIGINT(64) NOT NULL PRIMARY KEY,text varchar(500) CHARACTER SET utf8mb4,followers_count int,favorite_count INT,retweet_count INT,created_at varchar(255));
+
+CREATE TABLE TeslaTwitterAnalysisByOneDayFifteenMin(date VARCHAR(50) NOT NULL PRIMARY KEY,yyyymm CHAR(7), day INT,hour INT,quarters INT, median DOUBLE,mean DOUBLE,weighted_median DOUBLE,weighted_mean DOUBLE,count INT,zero_count INT);
+CREATE TABLE TeslaTwitterAnalysisByOneDaySixtyMin(date VARCHAR(50) NOT NULL PRIMARY KEY,yyyymm CHAR(7), day INT,hour INT,quarters INT, median DOUBLE,mean DOUBLE,weighted_median DOUBLE,weighted_mean DOUBLE,count INT,zero_count INT);
